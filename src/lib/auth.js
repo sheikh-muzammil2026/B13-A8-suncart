f806ce456c80d22c6b9ee034500fc7c820dc45e2
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-05-02 17:57:05 +0600

    fixed depoy problem seting variable in varcel dashboard

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,41 +1,45 @@
-// import { betterAuth } from "better-auth";
-// import { MongoClient } from "mongodb";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
-// const client = new MongoClient(process.env.MONGODB_URI);
-
-// await client.connect();
-// const db = client.db("suncart");
-
-
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(db, {
-    
-//     client
-//   }),
-//    emailAndPassword: { 
-//     enabled: true, 
-    
-//   },
-//   user: {
-//         fields: {
-//             image: "image", 
-//         }
-//     }
-// });
-
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
+
+await client.connect();
 const db = client.db("suncart");
 
+
+
 export const auth = betterAuth({
-  database: mongodbAdapter(db), // এখানে শুধু db দিলেই অনেক সময় কাজ করে
-  emailAndPassword: {
-    enabled: true,
+  database: mongodbAdapter(db, {
+    
+    client
+  }),
+  trustedOrigins: [
+        "https://b13-a8-suncart.vercel.app",
+        "http://localhost:3000"
+    ],
+   emailAndPassword: { 
+    enabled: true, 
+    
   },
-  // যদি ডিফল্ট ফিল্ডই ব্যবহার করেন, তবে user object রাখার প্রয়োজন নেই
+  user: {
+        fields: {
+            image: "image", 
+        }
+    }
 });
+
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+// const client = new MongoClient(process.env.MONGODB_URI);
+// const db = client.db("suncart");
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(db), // এখানে শুধু db দিলেই অনেক সময় কাজ করে
+//   emailAndPassword: {
+//     enabled: true,
+//   },
+//   // যদি ডিফল্ট ফিল্ডই ব্যবহার করেন, তবে user object রাখার প্রয়োজন নেই
+// });
