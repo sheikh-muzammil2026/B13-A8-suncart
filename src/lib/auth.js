c4b245e9885f8229ba1792fb01b5f7de0faaa05b
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-05-02 17:34:27 +0600

    try2

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,25 +1,41 @@
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+// const client = new MongoClient(process.env.MONGODB_URI);
+
+// await client.connect();
+// const db = client.db("suncart");
+
+
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(db, {
+    
+//     client
+//   }),
+//    emailAndPassword: { 
+//     enabled: true, 
+    
+//   },
+//   user: {
+//         fields: {
+//             image: "image", 
+//         }
+//     }
+// });
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-await client.connect();
 const db = client.db("suncart");
 
-
-
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    
-    client
-  }),
-   emailAndPassword: { 
-    enabled: true, 
-    
+  database: mongodbAdapter(db), // এখানে শুধু db দিলেই অনেক সময় কাজ করে
+  emailAndPassword: {
+    enabled: true,
   },
-  user: {
-        fields: {
-            image: "image", 
-        }
-    }
+  // যদি ডিফল্ট ফিল্ডই ব্যবহার করেন, তবে user object রাখার প্রয়োজন নেই
 });
